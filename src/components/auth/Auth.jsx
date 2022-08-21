a579bec3848b4ca4a4437d9587c83151dabd92d1
megomax88 <huku.ukuk@yandex.ru>
2022-08-21 18:44:29 +0300

    deploy last version

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -57,8 +57,8 @@ export default function Auth({ authState, setAuthState }) {
         <div className="mx-auto mt-5">
           <h2>Don't register yet?</h2>
           <Link to="/registration" className="btn btn-danger">Registration</Link>
-          <Link to="/news" className="btn btn-danger">СКИПАНУТЬ НА News</Link>
-          <Link to="/home" className="btn btn-danger">СКИПАНУТЬ НА ХОУМ</Link>
+          {/* <Link to="/news" className="btn btn-danger">СКИПАНУТЬ НА News</Link>
+          <Link to="/home" className="btn btn-danger">СКИПАНУТЬ НА ХОУМ</Link> */}
         </div>
       </form>
     </div>
